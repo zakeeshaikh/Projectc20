fb06ed8e002e1b12df5b89fc279d26aaf3b3df4a
Zakee Shaikh <78408518+zakeeshaikh@users.noreply.github.com>
2021-06-03 20:13:51 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -6,9 +6,9 @@ function preload() {
     catImg1 = loadImage("images/cat1.png");
     catImg2 = loadAnimation("images/cat2.png","images/cat3.png");
     catImg3 = loadAnimation("images/cat4.png");
-    mouseImg1 = loadImage("images/mouse4.png");
+    mouseImg1 = loadImage("images/mouse1.png");
     mouseImg2 = loadAnimation("images/mouse2.png","images/mouse3.png");
-    mouseImg3 = loadAnimation("images/mouse1.png");
+    mouseImg3 = loadAnimation("images/mouse4.png");
 
 }
 
